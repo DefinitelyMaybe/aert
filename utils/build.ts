745bf181b2f055c6a1b08687582d0ef7b5d2fee6
Rawr <DefinitelyMaybe@users.noreply.github.com>
2020-07-19 16:28:48 +1200

    wait for deno update

--- utils/build.ts
+++ utils/build.ts
@@ -3,12 +3,12 @@ const decoder = new TextDecoder()
 const [errors, emitted] = await Deno.bundle(
   "src/main.ts",
   {
-    "src/main.ts": 'document.getElementById("foo");'
-    //decoder.decode(Deno.readFileSync("src/main.ts"))
+    "src/main.ts": decoder.decode(Deno.readFileSync("src/main.ts"))
   },
   {
     lib: ["dom", "esnext"],
   },
 );
 
+// wait for deno 1.2.1
 console.log(emitted)
