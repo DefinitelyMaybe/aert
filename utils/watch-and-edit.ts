7a8d5ee0a31e96c21a64b7848685de633e7b1ce9
Rawr <DefinitelyMaybe@users.noreply.github.com>
2020-07-26 17:29:07 +1200

    updates example imports too

--- utils/watch-and-edit.ts
+++ utils/watch-and-edit.ts
@@ -14,8 +14,11 @@ async function main() {
 
       const match = text.match(/"three"/g)
       if (match) {
-        text = text.replace(/import .+?"three"/g, (m) => {
+        text = text.replaceAll(/import .+?"three"/g, (m) => {
           return `${m.slice(0, m.length-6)}./three.module.js"`
+        })
+        text = text.replaceAll(/"three\/examples\/jsm.+?"/g, (m) => {
+          return `"./${m.slice(16, m.length-1)}.js"`
         })
         data = encoder.encode(text)
         Deno.writeFileSync(event.paths[0], data)
