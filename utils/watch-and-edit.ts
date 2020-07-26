62c26afef533001b6dffa78c3dcbfc1d17410222
Rawr <DefinitelyMaybe@users.noreply.github.com>
2020-07-26 16:44:09 +1200

    auto updating built scripts urls

---
+++ utils/watch-and-edit.ts
@@ -0,0 +1,27 @@
+/// <reference path="./lib.deno.d.ts" />
+
+const decoder = new TextDecoder()
+const encoder = new TextEncoder()
+
+async function main() {
+  const watcher = Deno.watchFs("build");
+  for await (const event of watcher) {
+    console.log(">>>> event", event);
+    // { kind: "create", paths: [ "/foo.txt" ] }
+    if (event.paths[0] == "C:\\Users\\rekke\\Documents\\aert\\build\\main.js") {
+      let data = Deno.readFileSync("build/main.js");
+      let text = decoder.decode(data);
+
+      const match = text.match(/"three"/g)
+      if (match) {
+        text = text.replace(/import .+"three"/g, (m) => {
+          return `${m.slice(0, m.length-6)}./three.module.js"`
+        })
+        data = encoder.encode(text)
+        Deno.writeFileSync(event.paths[0], data)
+      }
+    }
+  }
+}
+
+main()
