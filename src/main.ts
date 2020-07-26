c8b9aa5fe6dde35c228041424d2baf1531b738b4
Rawr <DefinitelyMaybe@users.noreply.github.com>
2020-07-26 15:16:14 +1200

    update import url for npm

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
-import { Scene, PerspectiveCamera, WebGLRenderer } from './mod.ts'
+import { Scene, PerspectiveCamera, WebGLRenderer } from 'three'
 
 const scene = new Scene()
 const camera = new PerspectiveCamera(90, 1, 0.1, 100)
 const renderer = new WebGLRenderer({})
+
