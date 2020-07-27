2284346616edce7edbb24c80c9f0892e39cc735f
Rawr <DefinitelyMaybe@users.noreply.github.com>
2020-07-27 19:44:12 +1200

    fleshing out object controller script

--- src/controls/object.ts
+++ src/controls/object.ts
@@ -1,9 +1,10 @@
 class ObjectController {
   constructor() {
-    // Attach to an Object
-    // i.e. new ObjectController(Mesh)
+    // Attach to an Object and camera
+    // i.e. new ObjectController(mesh, camera)
 
     // Attach dom listeners for key presses
+    // move camera and object appropriately
 
     // Bonus features
     // enabled, speed
