dda05ebd5d0fb889f9adfda21be73c8670a7f326
Rawr <DefinitelyMaybe@users.noreply.github.com>
2020-07-27 19:33:59 +1200

    object controller

---
+++ src/controls/object.ts
@@ -0,0 +1,13 @@
+class ObjectController {
+  constructor() {
+    // Attach to an Object
+    // i.e. new ObjectController(Mesh)
+
+    // Attach dom listeners for key presses
+
+    // Bonus features
+    // enabled, speed
+  }
+}
+
+export { ObjectController }
