f8c26dd70c2b26dfd30a1a71e064ecf8a0d2a178
Rawr <DefinitelyMaybe@users.noreply.github.com>
2020-08-07 16:22:25 +1200

    renamed to follow

---
+++ src/controls/follow.ts
@@ -0,0 +1,14 @@
+class ObjectController {
+  constructor() {
+    // aiming for:
+    // const controls = new FollowControls(camera, renderer.domElement)
+    // controls.follow( object3d )
+
+    // perhaps simply extend the orbit controls
+    // but adjust it appropriately
+    // i.e. cannot move object, can rotate object to face camera direction
+
+  }
+}
+
+export { ObjectController }
