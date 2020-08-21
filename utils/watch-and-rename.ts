4ddf8bae723e70cb476df70c6a96da928542c107
Rawr <DefinitelyMaybe@users.noreply.github.com>
2020-08-22 10:21:41 +1200

    rename script

--- utils/watch-and-rename.ts
+++ utils/watch-and-rename.ts
@@ -1,7 +1,7 @@
 /// <reference path="./lib.deno.d.ts" />
 
-const decoder = new TextDecoder()
-const encoder = new TextEncoder()
+const decoder = new TextDecoder();
+const encoder = new TextEncoder();
 
 async function main() {
   const watcher = Deno.watchFs("build");
@@ -12,17 +12,17 @@ async function main() {
       let data = Deno.readFileSync("build/main.js");
       let text = decoder.decode(data);
 
-      const match = text.match(/"three"/g)
+      const match = text.match(/"three"/g);
       if (match) {
-        text = text.replaceAll(/"three"/g, `"./three.module.js"`)
+        text = text.replaceAll(/"three"/g, `"./three.module.js"`);
         text = text.replaceAll(/"three\/examples\/jsm.+?"/g, (m) => {
-          return `"./${m.slice(16, m.length-1)}.js"`
-        })
-        data = encoder.encode(text)
-        Deno.writeFileSync(event.paths[0], data)
+          return `"./${m.slice(16, m.length - 1)}.js"`;
+        });
+        data = encoder.encode(text);
+        Deno.writeFileSync(event.paths[0], data);
       }
     }
   }
 }
 
-main()
+main();
