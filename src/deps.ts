48ff3c8d34a2260545a24b8eeef99acad9769414
Rawr <DefinitelyMaybe@users.noreply.github.com>
2020-08-24 17:41:27 +1200

    create deps file

---
+++ src/deps.ts
@@ -0,0 +1,21 @@
+export {
+  Scene,
+  PerspectiveCamera,
+  WebGLRenderer,
+  BoxGeometry,
+  MeshStandardMaterial,
+  Mesh,
+  DirectionalLight,
+  Vector3,
+  PlaneBufferGeometry,
+  Raycaster,
+  Vector2,
+  Line,
+  BufferGeometry,
+  LineBasicMaterial,
+  Color,
+  HemisphereLight,
+  CameraHelper,
+} from "../libs/three.js/src/three.js";
+export { OrbitControls } from "../libs/three.js/examples/jsm/controls/OrbitControls.js";
+export { CannonPhysics } from "../libs/three.js/examples/jsm/physics/CannonPhysics.js";
