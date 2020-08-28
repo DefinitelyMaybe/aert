8e4637615a13e5af9437e41d1273236019f7737d
Rawr <DefinitelyMaybe@users.noreply.github.com>
2020-08-28 14:26:40 +1200

    remove extra files and folders

--- utils/libs-threejs-modForDeno.ts
+++ utils/libs-threejs-modForDeno.ts
@@ -1,26 +1,9 @@
-/*
-Overview:
-
-- Change examples/jsm import URLS
-- Add references to .d.ts files within respective .js files
-  - Update .d.ts import references to include file type
-
-extras:
-
-- Remove all of the .html files within the  examples folder
-
-*/
-
-// loop through libs/three.js/examples/jsm
-// find import { ... } from "../../../build/three.module.js"
-// replace with import { ... } from "../../../src/Three.js"
-// This is done only once, after three.js has been cloned.
-
 const decoder = new TextDecoder();
 const encoder = new TextEncoder();
 
 const filesToDelete: string[] = [];
 const examplesPath = "libs/three.js/examples/";
+const srcPath = "libs/three.js/src/";
 
 function loopDirAndMatch(path: string, pattern: RegExp, callBack: Function) {
   for (const dirEntry of Deno.readDirSync(path)) {
@@ -40,15 +23,37 @@ function addToDeleteList(path: string) {
   filesToDelete.push(path);
 }
 
-function updateScript(path: string) {
-  // update to include .d.ts in url
+function updatescripts(path: string) {
+  // update .d.ts script imports with deno appropriate urls
   let data = Deno.readFileSync(path);
   let text = decoder.decode(data);
-  const matches = text.matchAll(/import .+"/g);
+
+  // match the import
+  const matches = text.matchAll(/import .+;"/g);
   if (matches) {
     for (const match of matches) {
-      const newImport = `${match[0].slice(0, match[0].length - 1)}.d.ts"`;
-      text = text.replace(match[0], newImport);
+      console.log(match[0]);
+      // const newImport = `${match[0].slice(0, match[0].length - 1)}.d.ts"`;
+      // text = text.replace(match[0], newImport);
+    }
+  }
+  // write the new text to the same path
+  data = encoder.encode(text);
+  Deno.writeFileSync(path, data);
+}
+
+function updateTypescripts(path: string) {
+  // update .d.ts script imports with deno appropriate urls
+  let data = Deno.readFileSync(path);
+  let text = decoder.decode(data);
+
+  // match the import
+  const matches = text.matchAll(/import .+;"/g);
+  if (matches) {
+    for (const match of matches) {
+      console.log(match[0]);
+      // const newImport = `${match[0].slice(0, match[0].length - 1)}.d.ts"`;
+      // text = text.replace(match[0], newImport);
     }
   }
   // write the new text to the same path
@@ -69,7 +74,40 @@ function existsSync(path: string): boolean {
 }
 
 if (import.meta.main) {
-  // Delete extra .html files
+  // Delete the folders/files we're not using
+  const UnusedFilesAndFolders = [
+    "./libs/three.js/.github",
+    "./libs/three.js/build",
+    "./libs/three.js/docs",
+    "./libs/three.js/editor",
+    "./libs/three.js/examples/files",
+    "./libs/three.js/examples/fonts",
+    "./libs/three.js/examples/js",
+    "./libs/three.js/examples/models",
+    "./libs/three.js/examples/screenshots",
+    "./libs/three.js/examples/sounds",
+    "./libs/three.js/examples/textures",
+    "./libs/three.js/examples/files.js",
+    "./libs/three.js/examples/main.css",
+    "./libs/three.js/files",
+    "./libs/three.js/test",
+    "./libs/three.js/utils",
+    "./libs/three.js/.editorconfig",
+    "./libs/three.js/.gitattributes",
+    "./libs/three.js/.gitignore",
+    "./libs/three.js/icon.png",
+    "./libs/three.js/package.json",
+    "./libs/three.js/package-lock.json",
+    "./libs/three.js/README.md",
+  ];
+
+  UnusedFilesAndFolders.forEach((path) => {
+    if (existsSync(path)) {
+      Deno.removeSync(path, { recursive: true });
+    }
+  });
+
+  // Delete extra .html files within the examples folder
   loopDirAndMatch(examplesPath, /.html/g, addToDeleteList);
 
   if (filesToDelete.length != 0) {
@@ -80,31 +118,16 @@ if (import.meta.main) {
     });
   }
 
-  // Delete the folders we're not using
-  const UnusedFolders = [
-    "./libs/three.js/.github",
-    "./libs/three.js/build",
-    "./libs/three.js/docs",
-    "./libs/three.js/editor",
-    "./libs/three.js/files",
-    "./libs/three.js/test",
-    "./libs/three.js/utils",
-  ];
+  // Update .d.ts urls in remaining folders
+  loopDirAndMatch(examplesPath, /.d.ts/g, updateTypescripts);
+  loopDirAndMatch(srcPath, /.d.ts/g, updateTypescripts);
 
-  UnusedFolders.forEach((path) => {
-    if (existsSync(path)) {
-      Deno.removeSync(path);
-    }
-  });
+  // Update .js urls in the examples folder
+  // loopDirAndMatch(examplesPath, /.js/g, updatescripts);
+  // i.e.
+  // find import { ... } from "../../../build/three.module.js"
+  // replace with import { ... } from "../../../src/Three.js"
+  
+  // Add types reference to top of src/Three.js
+  // If needed, add '/// <reference lib="dom" />' to the top of src/Three.js
 }
-
-/*
-patterns
-some imports are single quoted i.e. '', others are double quoted ""
-import {
-  ...,
-  ...
-} from '...'
-
-import { ... } * from '...'
-*/
