1843e00161f38d4dd83369a7e97f44a669cd8a22
Rawr <DefinitelyMaybe@users.noreply.github.com>
2020-08-28 12:20:36 +1200

    remove scripts

--- utils/build-bundle.ts
+++
@@ -1,3 +0,0 @@
-/// <reference path="./lib.deno.d.ts" />
-// call deno bundle main.ts -> output to build folder
-// note: make sure you've got a deps.ts file keeping track of your dependencies
--- utils/build-copy-three.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="./lib.deno.d.ts" />
-// checkdir node_module/three.js
-// ensuredir build
-// copy three.module.js
-// copy jsm
