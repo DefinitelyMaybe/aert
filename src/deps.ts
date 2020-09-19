abe19d6907162c9437aa20d028ce57c9bae8291c
DefinitelyMaybe <rekked.noraa@gmail.com>
2020-09-19 16:51:53 +1200

    add pointerlock controls

--- src/deps.ts
+++ src/deps.ts
@@ -18,4 +18,5 @@ export {
   CameraHelper,
 } from "https://deno.land/x/threejs_4_deno@v120.3/src/Three.js";
 export { OrbitControls } from "https://deno.land/x/threejs_4_deno@v120.3/examples/jsm/controls/OrbitControls.js";
+export { PointerLockControls } from "https://deno.land/x/threejs_4_deno@v120.3/examples/jsm/controls/PointerLockControls.js";
 // export { AmmoPhysics } from "https://deno.land/x/threejs_4_deno@v120.3/examples/jsm/physics/AmmoPhysics.js";
