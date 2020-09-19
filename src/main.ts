934e4dc4d2ce05311b9beb1473bbeea867a2ad49
DefinitelyMaybe <rekked.noraa@gmail.com>
2020-09-20 10:05:15 +1200

    add resize function

--- src/main.ts
+++ src/main.ts
@@ -86,6 +86,14 @@ function animate() {
 animate();
 
 // UI & Events
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+window.addEventListener("resize", onWindowResize, false);
 
 const testButton = document.getElementById("test");
 testButton!.onclick = (e) => {
