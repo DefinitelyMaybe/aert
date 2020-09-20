38bc036c1edcbced3b4671e09f88743cc5674184
DefinitelyMaybe <rekked.noraa@gmail.com>
2020-09-20 17:28:52 +1200

    copy cannon

--- utils/build.ts
+++ utils/build.ts
@@ -1,11 +1,12 @@
-import { emptyDirSync, walkSync } from "https://deno.land/std/fs/mod.ts";
+import { existsSync, emptyDirSync, walkSync } from "https://deno.land/std/fs/mod.ts";
 
 const BUILD = "build/";
 const VIEWS = "views/";
 const SRC = "src/";
+const LIBS = "libs/"
 
 // remove everything from the build folder but make sure its there
-emptyDirSync(BUILD);
+// emptyDirSync(BUILD);
 
 // copy html and css
 const html = Deno.readTextFileSync(`${VIEWS}main.html`);
@@ -13,6 +14,13 @@ Deno.writeTextFileSync(`${BUILD}main.html`, html);
 
 const css = Deno.readTextFileSync(`${VIEWS}style.css`);
 Deno.writeTextFileSync(`${BUILD}style.css`, css);
+
+// copy all of libs into build
+// because libs shouldn't change much we can check if its already there
+if (!existsSync(`${BUILD}cannon.js`)) {
+  const cannon = Deno.readTextFileSync(`${LIBS}cannon.js`);
+  Deno.writeTextFileSync(`${BUILD}cannon.js`, cannon);
+}
 
 // compile src scripts with deno
 // @ts-ignore
