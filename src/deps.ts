191d1aa23aa840bfac8b0d78ff230edea8733a55
DefinitelyMaybe <rekked.noraa@gmail.com>
2020-09-20 17:27:43 +1200

    add clock and euler

--- src/deps.ts
+++ src/deps.ts
@@ -19,7 +19,9 @@ export {
   Quaternion,
   EventDispatcher,
   Object3D,
+  Euler,
+  Clock,
 } from "https://deno.land/x/threejs_4_deno@v120.3/src/Three.js";
 export { OrbitControls } from "https://deno.land/x/threejs_4_deno@v120.3/examples/jsm/controls/OrbitControls.js";
 export { PointerLockControls } from "https://deno.land/x/threejs_4_deno@v120.3/examples/jsm/controls/PointerLockControls.js";
-// export { AmmoPhysics } from "https://deno.land/x/threejs_4_deno@v120.3/examples/jsm/physics/AmmoPhysics.js";
+export { AmmoPhysics } from "https://deno.land/x/threejs_4_deno@v120.3/examples/jsm/physics/AmmoPhysics.js";
