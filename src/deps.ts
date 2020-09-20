d40ce3850874845f6209419aae19b53970ef2ee1
DefinitelyMaybe <rekked.noraa@gmail.com>
2020-09-20 19:04:30 +1200

    add init external cannon repo

--- src/deps.ts
+++ src/deps.ts
@@ -21,7 +21,9 @@ export {
   Object3D,
   Euler,
   Clock,
+  DirectionalLightHelper,
 } from "https://deno.land/x/threejs_4_deno@v120.3/src/Three.js";
 export { OrbitControls } from "https://deno.land/x/threejs_4_deno@v120.3/examples/jsm/controls/OrbitControls.js";
 export { PointerLockControls } from "https://deno.land/x/threejs_4_deno@v120.3/examples/jsm/controls/PointerLockControls.js";
-export { AmmoPhysics } from "https://deno.land/x/threejs_4_deno@v120.3/examples/jsm/physics/AmmoPhysics.js";
+// @deno-types="https://unpkg.com/cannon-es/dist/cannon-es.d.ts" />
+export { World, NaiveBroadphase, AABB, Box, Body, Plane } from 'https://unpkg.com/cannon-es/dist/cannon-es.js'
