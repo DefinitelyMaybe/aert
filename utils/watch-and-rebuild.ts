6a2f193e8f384d85368d900e6410433429702a2c
DefinitelyMaybe <rekked.noraa@gmail.com>
2020-09-25 08:52:38 +1200

    deno fmt

--- utils/watch-and-rebuild.ts
+++ utils/watch-and-rebuild.ts
@@ -1,9 +1,8 @@
-
 const watcher = Deno.watchFs("src");
 
 for await (const event of watcher) {
   console.log(">>>> event", event);
   // { kind: "create", paths: [ "/foo.txt" ] }
   // console.log(Deno.cwd());
-  Deno.run({cmd: ["deno", "run", "-A", "--unstable", "utils/build.ts"]})
+  Deno.run({ cmd: ["deno", "run", "-A", "--unstable", "utils/build.ts"] });
 }
