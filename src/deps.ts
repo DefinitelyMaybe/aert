71b069158d678cb6ebe507ce5862d07be4e54294
DefinitelyMaybe <rekked.noraa@gmail.com>
2020-09-26 21:48:25 +1200

    add spherical

--- src/deps.ts
+++ src/deps.ts
@@ -23,6 +23,7 @@ export {
   Clock,
   DirectionalLightHelper,
   Camera,
+  Spherical,
 } from "https://deno.land/x/threejs_4_deno@v120.3/src/Three.js";
 export { OrbitControls } from "https://deno.land/x/threejs_4_deno@v120.3/examples/jsm/controls/OrbitControls.js";
 export { PointerLockControls } from "https://deno.land/x/threejs_4_deno@v120.3/examples/jsm/controls/PointerLockControls.js";
