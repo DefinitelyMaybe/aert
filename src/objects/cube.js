64b158376f9fbced6d94e9b272efc0e1bd416208
DefinitelyMaybe <rekked.noraa@gmail.com>
2020-11-20 12:20:33 +1300

    working draft of cube object

--- src/objects/cube.js
+++ src/objects/cube.js
@@ -1,30 +1,30 @@
-import { BoxGeometry, Box, Body } from "../deps.js"
+import {
+  Body,
+  Box,
+  BoxGeometry,
+  Mesh,
+  MeshStandardMaterial,
+  Vec3,
+} from "../deps.js";
 
-export class Cube extends BoxGeometry {
-  constructor(
-    width,
-    height,
-    depth,
-    widthSegments,
-    heightSegments,
-    depthSegments,
-  ){
-    // super creates the BoxGeometry
-    super(
-      width,
-      height,
-      depth,
-      widthSegments,
-      heightSegments,
-      depthSegments,
-    )
-    
-    // I simply add the physics
-    const cube = new Box(new Vec3(width, height, depth));
+export class Cube extends Mesh {
+  defaultMaterial = new MeshStandardMaterial({ color: 0xaaaaaa });
+
+  constructor(options = {}) {
+    super();
+    Object.defineProperty(this, "isCube", { value: true });
+    this.depth = options.depth ? options.depth : 1;
+    this.width = options.width ? options.width : 1;
+    this.height = options.height ? options.height : 1;
+
+    this.geometry = new BoxGeometry(this.width, this.depth, this.height);
+    this.material = options.material ? options.material : this.defaultMaterial;
+
+    // add physics
+    const cube = new Box(
+      new Vec3(this.width / 2, this.height / 2, this.depth / 2),
+    );
     this.body = new Body({ mass: 1 });
     this.body.addShape(cube);
-    
-    // this.userData.physics.body = cubeBody
-    
   }
 }
