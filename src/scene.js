3c833c3e5450390af4dc1ad3f8d3af1408c89683
DefinitelyMaybe <rekked.noraa@gmail.com>
2020-11-20 12:22:51 +1300

    world adds objects to it that have physics bodies

--- src/scene.js
+++ src/scene.js
@@ -1,17 +1,19 @@
-import { Scene } from "./deps.js";
+import { Scene as ThreeScene } from "./deps.js";
 import { world } from "./main.js";
 
-class MyScene extends Scene {
-  constructor(){
-    super()
+export class Scene extends ThreeScene {
+  constructor() {
+    super();
   }
 
-  add(obj){
-    if (obj.userdata.physics) {
-      super.add(obj)
-      world.addBody(obj.userdata.physics.body);
+  add(obj) {
+    if (obj.body) {
+      // add the physics
+      super.add(obj);
+      world.addBody(obj.body);
     } else {
-      console.error("My Scene tried to add an object without physics obj attached")
+      // normal add
+      super.add(obj);
     }
   }
 }
