a72f4d97705e056b620a3af41edd7284b976f65e
DefinitelyMaybe <rekked.noraa@gmail.com>
2021-02-11 13:02:11 +1300

    deno type ref

--- src/deps.ts
+++ src/deps.ts
@@ -30,7 +30,9 @@ export {
 } from "https://cdn.skypack.dev/three?dts";
 // continue to export from here because these files really should be from the import above
 // they're not yet. must wait for es module support across examples folder of three.js
+// @deno-types="./controls/OrbitControls.d.ts"
 export { OrbitControls } from "./controls/OrbitControls.js";
+// @deno-types="./controls/PointerLockControls.d.ts"
 export { PointerLockControls } from "./controls/PointerLockControls.js";
 export {
   AABB,
@@ -44,3 +46,4 @@ export {
   Vec3,
   World,
 } from "https://cdn.skypack.dev/cannon-es";
+export * as tweakpane from 'https://cdn.skypack.dev/tweakpane';
