87515a0fbe0bc518f4172fcfbc2e7c03affd2d55
DefinitelyMaybe <rekked.noraa@gmail.com>
2021-02-15 11:47:25 +1300

    remove mesh param from controls init

--- src/objects/player.ts
+++ src/objects/player.ts
@@ -2,7 +2,6 @@
 import { PlayerControls } from "../controls/PlayerControls.ts";
 import { Cube } from "./cube.ts";
 import { MeshStandardMaterial, CircleGeometry, Object3D } from "../deps.ts";
-import type { Scene } from "../scene.ts"
 
 // controls trigger events here
 // access the model
@@ -24,11 +23,7 @@ export class Player extends Object3D {
     this.camera = camera
     this.domEl = domEl
 
-    this.controls = new PlayerControls(this.mesh, camera, domEl)
+    this.controls = new PlayerControls(camera, domEl)
     // this.circle = new CircleGeometry(2, 5, Math.PI/4, Math.PI/2)
-  }
-
-  addtoScene(scene:Scene) {
-    scene.add(this.circle)
   }
 }
