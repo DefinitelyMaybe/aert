9da943ed29c96aed4414cf3db768f95904a33566
DefinitelyMaybe <rekked.noraa@gmail.com>
2021-02-15 09:10:46 +1300

    extend Object3D

--- src/objects/player.ts
+++ src/objects/player.ts
@@ -1,22 +1,30 @@
 // handles the player object appropriately
 import { PlayerControls } from "../controls/PlayerControls.ts";
 import { Cube } from "./cube.ts";
-import { MeshStandardMaterial, CircleGeometry } from "../deps.ts";
+import { MeshStandardMaterial, CircleGeometry, Object3D } from "../deps.ts";
 import type { Scene } from "../scene.ts"
 
 // controls trigger events here
 // access the model
-export class Player {
+// A Player has a Mesh and a controller for that mesh.
+// The controller will 
+export class Player extends Object3D {
+  domEl
+  camera
   controls
-  object
+  mesh
   circle
   constructor(camera, domEl) {
-    this.object = new Cube({ material: new MeshStandardMaterial({ color: 0x00ff00 }) })
-    this.object.castShadow = true;
-    this.object.receiveShadow = true;
-    this.object.name = "player";
+    super()
+    this.mesh = new Cube({ material: new MeshStandardMaterial({ color: 0x00ff00 }) })
+    this.mesh.castShadow = true;
+    this.mesh.receiveShadow = true;
+    this.mesh.name = "player";
 
-    this.controls = new PlayerControls(this.object, camera, domEl)
+    this.camera = camera
+    this.domEl = domEl
+
+    this.controls = new PlayerControls(this.mesh, camera, domEl)
     this.circle = new CircleGeometry(2, 5, Math.PI/4, Math.PI/2)
   }
 
