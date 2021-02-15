108b6fcd75c661bf334353ac830f3628adcc9021
DefinitelyMaybe <rekked.noraa@gmail.com>
2021-02-16 10:16:07 +1300

    extend thirdpersoncontrols

--- src/controls/TopDownControls.ts
+++ src/controls/TopDownControls.ts
@@ -1,8 +1,9 @@
 import type { PerspectiveCamera } from "../deps.ts";
-import { MapControls } from "./MapControls.ts";
+import type { Cube } from "../objects/cube.ts";
+import { ThirdPersonControls } from "./thirdPersonControls.ts";
 
-export class TopDownControls extends MapControls {
-  constructor(cam: PerspectiveCamera, domEl: HTMLElement) {
-    super(cam, domEl);
+export class TopDownControls extends ThirdPersonControls {
+  constructor(object:Cube, cam: PerspectiveCamera, domEl: HTMLElement) {
+    super(object, cam, domEl);
   }
 }
