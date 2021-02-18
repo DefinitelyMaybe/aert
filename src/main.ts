ee76369a00aadc31a38a10201904ef17c5d54af2
DefinitelyMaybe <rekked.noraa@gmail.com>
2021-02-19 11:25:40 +1300

    positioning of tweakpane for world init

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,16 @@ import { Tweakpane } from "./deps.ts";
 import { DevWorld } from "./worlds/devworld.ts";
 
 // ---------------- Variables --------------------
-const world = new DevWorld()
-
 // tweakpane
 export const pane = new Tweakpane.default();
+const tsave = pane.addButton({title:"save"})
+tsave.on("click", ()=> {
+  world.save()
+})
+
+const world = new DevWorld()
 
 // ---------------- Functions --------------------
 
 
-// Events
+// ---------------- Events --------------------
